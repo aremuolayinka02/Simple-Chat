3bdeb14958ef6e559774218a0623f7fca97387af
Olayinka.Dev <aremuolayinka02@gmail.com>
2022-10-12 07:55:20 +0100

    uninstall react-router-dom - not necessary for small scale app

--- src/Chat.js
+++ src/Chat.js
@@ -6,6 +6,8 @@ export const Chat = () => {
     <div className="chat__room">
       <p className="chat__header">CHAT ROOM</p>
 
+      <div className="chats__section"></div>
+
       <div className="chat__send">
         <input type="text" />
         <button>Send</button>
